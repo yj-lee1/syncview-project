e22ce0cc47633b4f196424c18d85bab495e311b6
yj-lee1 <kakat9516@gmail.com>
2025-06-05 11:45:05 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-// src/App.js
+// src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import News from "./pages/News";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup"; // 회원가입 페이지 파일이 있다면
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/news" element={<News />} />
       </Routes>
     </BrowserRouter>
   );
