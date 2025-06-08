7e8179bb6e3baa8ad70e906b750c52003f609301
JoYuYoung <t01047852048@gmail.com>
2025-06-09 03:33:25 +0900

    db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,16 @@
 // backend/db.js
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",        // ✅ pgAdmin에서 사용 중인 사용자
-  host: "localhost",
-  database: "syncview",    // ✅ 네가 만든 데이터베이스 이름
-  password: "2048",     // ✅ 네 Postgres 비번
-  port: 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false // GCP 외부 접속 시 필요
+  }
 });
 
 module.exports = pool;
